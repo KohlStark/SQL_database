f1414024d6e69b58b36a004050dd1ef02514bde1
KohlStark <45770597+KohlStark@users.noreply.github.com>
2018-12-17 01:05:53 -0700

    Create drop.sql

---
+++ drop.sql
@@ -0,0 +1,18 @@
+/*
+Kohl Stark
+kgstark1@asu.edu
+*/
+
+/*
+** ----------------------------------------------------------------------------
+** script to drop all objects in the database
+** --------------------------------------------------------------------------*/
+
+-- Tables 
+DROP TABLE Managers purge;
+DROP TABLE Servers purge;
+DROP TABLE Cooks purge;
+DROP TABLE Bartenders purge;
+DROP TABLE Hosts purge;
+DROP TABLE Bussers purge;
+DROP TABLE Restaurant_Table purge;
